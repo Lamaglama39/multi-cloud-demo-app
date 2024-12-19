e26f37d1511561b819e2388b09c8c1705b0d6811
Lamaglama39 <lamaglama621@gmail.com>
2024-12-19 23:05:03 +0900

    :construction: add azure resource group module

---
+++ app/modules/azure/rg/variables.tf
@@ -0,0 +1,9 @@
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "location" {
+  description = "The location/region where the resource group will be created"
+  type        = string
+}
