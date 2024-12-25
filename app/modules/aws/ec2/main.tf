66330cd1cef210e8c76deeed45b44a64be5559a6
Lamaglama39 <lamaglama621@gmail.com>
2024-12-25 22:38:35 +0900

    :construction: add aws ec2 module

---
+++ app/modules/aws/ec2/main.tf
@@ -0,0 +1,17 @@
+module "ec2" {
+  source   = "terraform-aws-modules/ec2-instance/aws"
+  for_each = var.server_instances_map
+
+  name          = "${var.app_name}-${var.subnet_id}-ec2"
+  instance_type = each.value.instance_type
+  private_ip    = each.value.private_ip
+
+  iam_instance_profile   = var.iam_role
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+
+  ami                         = each.value.ami
+  associate_public_ip_address = each.value.associate_public_ip_address
+  user_data                   = each.value.user_data
+}
