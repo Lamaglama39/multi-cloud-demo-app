32ebdfeb35b65894ce1cc3d658c1753cdf43c0f9
Lamaglama39 <lamaglama621@gmail.com>
2025-01-05 21:41:00 +0900

    :construction: add aws cloudfront module

---
+++ app/modules/aws/cloudfront/main.tf
@@ -0,0 +1,45 @@
+data "aws_cloudfront_cache_policy" "main" {
+  name = "Managed-CachingDisabled"
+}
+
+data "aws_cloudfront_origin_request_policy" "main" {
+  name = "Managed-AllViewer"
+}
+
+module "cdn" {
+  source = "terraform-aws-modules/cloudfront/aws"
+
+  comment             = "CloudFront with ALB Origin (No Cache)"
+  enabled             = true
+  price_class         = "PriceClass_All"
+  retain_on_delete    = false
+  wait_for_deployment = false
+
+  origin = {
+    alb = {
+      domain_name = var.alb_domain_name
+      origin_id   = "alb-origin"
+
+      custom_origin_config = {
+        http_port              = 80
+        https_port             = 443
+        origin_protocol_policy = "http-only"
+        origin_ssl_protocols   = ["TLSv1.2"]
+      }
+    }
+  }
+
+  default_cache_behavior = {
+    target_origin_id       = "alb-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    use_forwarded_values   = false
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods  = ["GET", "HEAD"]
+
+    cache_policy_id          = data.aws_cloudfront_cache_policy.main.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.main.id
+
+    compress = true
+  }
+}
---
+++ app/modules/aws/cloudfront/variables.tf
@@ -0,0 +1,7 @@
+variable "app_name" {
+  type = string
+}
+
+variable "alb_domain_name" {
+  type = string
+}
